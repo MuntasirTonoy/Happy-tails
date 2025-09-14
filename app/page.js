34d64a472895043bbf24ce92f04ef693df101ca9
tanzia mourin <tanziamourin68@gmail.com>
2025-09-14 13:36:36 +0600

    frist commit

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         <span className="btn btn-secondary btn-xl rounded-2xl ">
           @/public/data
         </span>
-        fetch those and use for designing
+        fetch those and use for designing .
       </h2>
     </div>
   );
